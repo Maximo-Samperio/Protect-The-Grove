ed9c9d4443eace932b176e4d03f71af1532505d2
Maximo Samperio <maximosamperio01@gmail.com>
2023-09-07 14:31:32 -0300

    Added shooting and tracking to turrets

--- Protect The Grove/Assets/Scripts/Cannonball.cs
+++ Protect The Grove/Assets/Scripts/Cannonball.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Cannonball : MonoBehaviour
 {
     private Transform target;
+    const float speed = 20f;
+    public GameObject impactEffect;
 
 
     public void Seek (Transform _target)
@@ -14,13 +17,30 @@ public class Cannonball : MonoBehaviour
 
     void Update()
     {
-        if (target != null)
+        //if (target != null)
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+
+        Vector3 dir = target.position - transform.position;         // Direction the bullet needs to look at
+        float distanceThisFrame = speed * Time.deltaTime;           // Actual distance to move per frame relative to the set speed
+
+        if (dir.magnitude <= distanceThisFrame)                     // To check if it has hit an target
         {
-            Destroy(gameObject);
+            HitTarget();
             return;
         }
 
-        Vector3 dir = target.position - transform.position;         // Direction the bullet needs to look at
-
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);    // Normal to make speed constant
     }
+
+    void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);      // I play the particle effect
+        Destroy(effectIns, 2f);
+        
+        Destroy(gameObject);                                        // I destroy the bullet
+    }
+
 }
